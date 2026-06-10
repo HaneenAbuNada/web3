ff0169f6fed1565cdf779e59463d047eb96cf990
Sana <sana.mousa96@gmail.com>
2026-06-10 12:34:45 +0300

    Create Laravel project

---
+++ resources/views/about.blade.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About - Web Programming III</title>
+</head>
+<body>
+    <h1>Welcome to Web Programming III</h1>
+    <p>This is the Week 1 practice exercise.</p>
+</body>
+</html>
