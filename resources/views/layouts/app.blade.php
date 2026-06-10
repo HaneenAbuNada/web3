0c5349c432354636c11b665b854e08d8e4217a92
Haneen AbuNada <EMAIL_OF_HANEEN>
2026-06-10 13:17:42 +0300

    Week 2 assignment

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>@yield('title')</title>
+</head>
+<body>
+    <h1>Web Programming III</h1>
+    <hr>
+
+    @yield('content')
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/about', [PageController::class, 'about']);
+Route::get('/students', [StudentController::class, 'index']);
